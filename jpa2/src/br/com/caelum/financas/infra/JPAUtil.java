cd94640dbb1645fc65d4f55807cbbbcede83a097
TenSoja <tensoja@gmail.com>
2012-10-29 12:49:06 -0200

    adicionada JPAUtil que encapsula chamadas a EntityManagerFactory

---
+++ jpa2/src/br/com/caelum/financas/infra/JPAUtil.java
@@ -0,0 +1,16 @@
+package br.com.caelum.financas.infra;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+	
+	private static EntityManagerFactory entityManagerFactory = Persistence
+			.createEntityManagerFactory("controlefinancas");
+	
+	public EntityManager getEntityManager() {
+		return entityManagerFactory.createEntityManager();
+		
+	}
+}
