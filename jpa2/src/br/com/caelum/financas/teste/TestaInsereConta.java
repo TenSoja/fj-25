c17b797b67d0224ecfbecc9042278da06d5b6dac
TenSoja <tensoja@gmail.com>
2012-10-29 12:58:54 -0200

    Alterada para trabalhar com JPAUtil.

--- jpa2/src/br/com/caelum/financas/teste/TestaInsereConta.java
+++ jpa2/src/br/com/caelum/financas/teste/TestaInsereConta.java
@@ -4,29 +4,35 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import br.com.caelum.financas.infra.JPAUtil;
 import br.com.caelum.financas.modelo.Conta;
 
 public class TestaInsereConta {
 	public static void main(String[] args) {
-		
+
 		long inicio = System.currentTimeMillis();
 		
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("controlefinancas");
-		EntityManager entityManager = factory.createEntityManager();
+		/*
+		 * EntityManagerFactory factory =
+		 * Persistence.createEntityManagerFactory("controlefinancas");
+		 * EntityManager entityManager = factory.createEntityManager();
+		 */
 		
+		EntityManager entityManager = new JPAUtil().getEntityManager();
+
 		Conta conta = new Conta();
 		conta.setTitular("José Roberto");
 		conta.setBanco("Banco do Brasil");
 		conta.setNumero("123456-6");
 		conta.setAgencia("0999");
-		
+
 		entityManager.getTransaction().begin();
 		entityManager.persist(conta);
 		entityManager.getTransaction().commit();
 		entityManager.close();
-		
+
 		System.out.println("Conta gravada com sucesso!");
 		long fim = System.currentTimeMillis();
-		System.out.println("Executado em:"+(fim - inicio));
+		System.out.println("Executado em:" + (fim - inicio));
 	}
 }
